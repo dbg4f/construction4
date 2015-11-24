24366a68322735c102f1293412b3ab07851dc80b
dbg4f <oismae3718>
2015-11-24 12:30:43 +0200

    Bricking draft

--- src/dbg/construction/bricking/draw/ProjectionHelper.java
+++ src/dbg/construction/bricking/draw/ProjectionHelper.java
@@ -1,10 +1,17 @@
 package dbg.construction.bricking.draw;
 
+import dbg.construction.bricking.Brick;
+import dbg.construction.geometry.CommonPlane;
+
 /**
  * @author bogdel on 23.11.15.
  */
 public class ProjectionHelper {
 
+
+    public static String projection(Brick brick, CommonPlane commonPlane, double scale) {
+        return new SvgRect(brick.getCenter(), brick.getDimensions(), commonPlane).toSvg(scale);
+    }
 
 
 }
